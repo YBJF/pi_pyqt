a2b0c624b6278d0c45daa88964d803a56b57b2f1
YBJF <778349156@qq.com>
2021-01-28 16:57:20 +0800

    Add files via upload

--- test_1.py
+++ test_1.py
@@ -1,16 +1,22 @@
+ #coding: UTF-8
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import sys
 import os
 
-def my_click():
+def button1():
 	os.system("xdg-open ~/2.mp4")
+ 
+def button4():
+	sys.exit()
 
 if __name__=='__main__':
 	app=QApplication(sys.argv)
 	window=QWidget()
 	window.setWindowTitle("test")
+	window.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowSystemMenuHint)
 	screen=QDesktopWidget().screenGeometry()
 	window.resize(screen.width(),screen.height())
 	
@@ -35,19 +41,24 @@ if __name__=='__main__':
 
 	#button
 	btn1=QPushButton("视频")
-	btn1.setGeometry(screen.width()-600,screen.height()-100,100,50)
+	btn1.setGeometry(screen.width()-400,screen.height()-100,100,50)
 	btn1.setParent(window)
 
 	btn2=QPushButton("分类")
-	btn2.setGeometry(screen.width()-400,screen.height()-100,100,50)
+	btn2.setGeometry(screen.width()-300,screen.height()-100,100,50)
 	btn2.setParent(window)
 
 	btn3=QPushButton("满载")
 	btn3.setGeometry(screen.width()-200,screen.height()-100,100,50)
 	btn3.setParent(window)
+ 
+	btn4=QPushButton("退出")
+	btn4.setGeometry(screen.width()-100,screen.height()-100,100,50)
+	btn4.setParent(window)
 
 	window.show()
 
-	btn1.clicked.connect(my_click)
+	btn1.clicked.connect(button1)
+	btn4.clicked.connect(button4)
     
 	sys.exit(app.exec())
